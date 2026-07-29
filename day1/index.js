db91dc2029a5b688ce8e95bd1f374ecd9d53ee81
ShristiDuhhon <shristiduhoon24@gmail.com>
2026-07-29 14:58:26 +0530

    first

---
+++ day1/index.js
@@ -0,0 +1,29 @@
+const app=require("./app.js")
+
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b/c);
+
+
+// var x=parseInt(prompt("Enter value: "));
+// if(x<20){
+//     console.log("x is "+x);
+// }
+// else{
+//     console.log("x is greater than 20");
+// }
+
+// for(var i=0;i<6;i++){
+//     console.log(i);
+// }
+
+// var x=30;
+// const arr=[3,7,5,2,9];
+// for(var i=0;i<5;i++){
+//     console.log(arr[i]);
+    
+// }
+// console.log(arr[4]);
+
+console.log(app.xz);
