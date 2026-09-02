5852c3742807209c3bb2e93f5112980951b69869
ShristiDuhhon <shristiduhoon24@gmail.com>
2026-09-02 14:48:59 +0530

    day5

---
+++ day5.js
@@ -0,0 +1,53 @@
+// const a={
+//     name:"shristi",job:"teacher",
+//     details:function(){
+//         console.log("My name is:"+this.name)
+//         console.log("My job is:"+this.job)
+//     },
+//     profile:"assistant professor",
+// }
+
+// const a={
+//     name:"shristi",salary:15000,
+//     display:function(){
+//         console.log("My name is:"+this.name)
+//         console.log("My salary is:"+this.salary)
+//     },
+//     inc_sal:function(){
+//         this.salary=this.salary+5000
+//         console.log("My salary is:"+this.salary)
+//     }
+// }
+// a.display()
+// a.inc_sal()
+
+// const student={
+//     name:"shristi",marks:21,
+//     inc_marks:function(){
+//         this.marks=this.marks+5
+//     },
+//     display:function(){
+//         console.log("My marks is:"+this.marks)
+//     }
+// }
+
+// student.inc_marks()
+// student.display()
+
+// function show(){
+//     console.log("My address is:"+this.address)
+//     console.log("My city is:"+this.city)
+// }
+// e={address:"Abes college",city:"ghaziabad",show:show}
+// f={address:"college of engineering near crossing",city:"ghaziabad",show:show}
+// e.show()
+// f.show()
+
+
+// function clg(){
+//     const dep={name:"ai",id:032}
+//     const class={name:"a",id:1}
+// }
+
+// console.log("name :"+this.name)
+//     console.log("id:"+this.id)
